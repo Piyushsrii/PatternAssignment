91f1ccef447d5c0c1e73e24d04b90a423057cf0e
piyushsrii <piyushnamo888@gmail.com>
2020-07-24 00:50:49 +0530

    commit in uc3

--- PatternUserCase.sh
+++ PatternUserCase.sh
@@ -1,1 +1,12 @@
 Welcome
+
+#!/bin/bash -x
+echo "Enter Your First name"
+read code
+pattern="^[a-z]*[.]+[a-z]*[@]+[a-z]*[.]+[a-z]{2,}[.]+[a-z]{2,}$"
+if [[ $code =~ $pattern ]]
+then
+echo found
+else
+echo not found
+fi
