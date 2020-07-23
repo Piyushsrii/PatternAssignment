3618b7d8cfa37a44798f13eabadbec348f25db81
piyushsrii <piyushnamo888@gmail.com>
2020-07-24 01:05:43 +0530

    commit in uc8

--- PatternUserCase.sh
+++ PatternUserCase.sh
@@ -1,1 +1,11 @@
 Welcome
+
+#!/bin/bash -x
+echo "enter password code"
+read str
+if [[ ${#str} -ge 8 && "$str"==[[:lower:]]+ && "$str"==[[:upper:]]+ && "$str"==[0-9]+ ]]
+then
+echo found
+else
+echo not found
+fi
