e0ffa018229323988b9f361e4d16893ec8736eaf
piyushsrii <piyushnamo888@gmail.com>
2020-07-24 01:16:05 +0530

    commit in uc9

--- PatternUserCase.sh
+++ PatternUserCase.sh
@@ -1,6 +1,6 @@
 Welcome
 
-#!/bin/bash -x
+#!/bin/bash -x         UserCase------->9
 echo "enter password code"
 read str
 if [[ ${#str} -ge 8 && "$str"==[[:lower:]]+ && "$str"==[[:upper:]]+ && "$str"==[0-9]+ && "$str"==[@#%^-*+/]+ ]]
